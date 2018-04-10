a949df79278698486791650543e547098105d0bf
Nguyễn Văn Toàn <toan210597ntu@gmail.com>
2018-04-10 09:28:47 +0700

    Add files via upload

---
+++ tinh.sh
@@ -0,0 +1,37 @@
+echo "nhap a "
+read a
+echo "nhap b"
+read b
+let t=$a+$b
+echo "tong la : $t"
+echo "$a + $b = "`expr $a + $b`
+if [ $a -eq $b ];
+then 
+  echo "$a bang $b"
+elif [ $a -gt $b ];
+  then
+    echo $a
+elif [ $a -lt $b ];
+  then 
+    echo $b
+else
+   echo "khong co so so sanh"
+fi
+echo "nhap vao n :"
+read n
+s=0
+for((i=0;i<=n;i++))
+do
+let "s= $s + $i"
+done
+echo "tong la: $s"
+echo "nhap k: "
+read k
+let "s = $a / $b "
+echo "scale =2;$s" | bc
+function tong(){
+let "s = $s1 + $s2 "
+echo "tong : $s"
+}
+
+
